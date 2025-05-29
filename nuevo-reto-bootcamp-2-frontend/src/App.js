e7236e6b732ee3b584c27dff507175cad8c5dbfd
José Eduardo Ormeño Meneses <eduardoorm79@gmail.com>
2025-05-29 15:20:47 -0500

    feat: added frontend and backend service

---
+++ nuevo-reto-bootcamp-2-frontend/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+    fetch('http://localhost:3000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.error(err));
+  }, []);
+  
+  return (
+    <div className="App">
+      <h1>Lista de Usuarios</h1>
+      <div className="user-list">
+        {users.map((user, index) => (
+          <div className="user-card" key={index}>
+            <img src={user.fotografia} alt="User" />
+            <h3>{user.nombre}</h3>
+            <p>Género: {user.genero}</p>
+            <p>Ubicación: {user.ubicacion}</p>
+            <p>Correo: {user.correo}</p>
+            <p>Fecha de nacimiento: {new Date(user.fechaNacimiento).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
